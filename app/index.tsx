7c0b9a1813cbb2f037fd5723acc181af692cb458
Jose Medina <jomedina@microsoft.com>
2016-09-29 22:39:26 -0700

    Creating classes for header and menu options

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,46 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-ReactDOM.render(<div>test</div>, document.getElementById('app'));
+class MenuOption extends React.Component<{}, {}> {
+  render() {
+    return <li>{this.props.children}</li>
+  }
+}
+class Menu extends React.Component<{}, {}> {
+  render() {
+    return <ul>{this.props.children}</ul>
+  }
+}
+
+class Container extends React.Component<{}, {}>{
+  render() {
+    return <div>{this.props.children}</div>
+  }
+}
+
+class HeaderTitle extends React.Component<{}, {}> {
+  render() {
+    return <h1>{this.props.children}</h1>
+  }
+}
+
+class Header extends React.Component<{}, {}> {
+  render() {
+    return <Container>{this.props.children}</Container>;
+  }
+}
+
+
+
+ReactDOM.render(
+  <Container>
+    <Header>
+      <HeaderTitle>Jose &amp; Mercedes</HeaderTitle>
+      <Menu>
+        <MenuOption>One</MenuOption>
+        <MenuOption>Two</MenuOption>
+        <MenuOption>Three</MenuOption>
+      </Menu>
+    </Header>
+  </Container>
+  , document.getElementById('app'));
