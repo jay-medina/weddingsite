0c6f78e70bc8396633aa75a5b10bce41c6369c15
Jose Medina <josemedina921@gmail.com>
2016-10-29 23:23:13 -0700

    adding the scroll effect

--- app/views/WeddingHeader.tsx
+++ app/views/WeddingHeader.tsx
@@ -1,15 +1,38 @@
 import * as React from 'react';
+import * as $ from 'jquery';
 import { MenuOption, Menu, Header } from './Header';
 
-export function WeddingHeader() {
-  return (
-    <Header>
-      <Menu>
-        <MenuOption link="/" >Our Story</MenuOption>
-        <MenuOption link="/whenandwhere" >When &amp; Where</MenuOption>
-        <MenuOption link="/registry" >Registry</MenuOption>
-        <MenuOption link="/rsvp" >RSVP</MenuOption>
-      </Menu>
-    </Header>
-  );
+function scrollHandler($el: JQuery) {
+  const originalTop = $el.offset().top;
+  
+  $(window).off('scroll')
+            .on('scroll', () => {
+              const currentOffset = $el.offset();
+
+              if(currentOffset.top >= originalTop + 50) {
+                $el.addClass('scrolled');
+              }
+              else {
+                $el.removeClass('scrolled');
+              }
+  });
+}
+
+export class WeddingHeader extends React.Component<{},{}> {
+  render() {
+    return (
+      <Header>
+        <Menu>
+          <MenuOption link="/" >Our Story</MenuOption>
+          <MenuOption link="/whenandwhere" >When &amp; Where</MenuOption>
+          <MenuOption link="/registry" >Registry</MenuOption>
+          <MenuOption link="/rsvp" >RSVP</MenuOption>
+        </Menu>
+      </Header>
+    );
+  }
+  componentDidMount() {
+    const $header = $('.wedding_header');
+    scrollHandler($header);
+  }
 }
